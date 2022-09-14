a7b3a361f5fb5f7ac647ed523136d726cca5fc50
libsgh <woiyyng@gmail.com>
2022-09-14 17:28:24 +0800

    update

--- TiebaSign/db.go
+++ TiebaSign/db.go
@@ -16,6 +16,7 @@ var Db *gorm.DB
 func init() {
 	var err error
 	dialector := postgres.Open("port=5432 host=localhost user=xx password=xxxa dbname=xxx")
+	fmt.Println("DATABASE_URL: " + os.Getenv("DATABASE_URL"))
 	if os.Getenv("DATABASE_URL") != "" {
 		db, _ := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 		dialector = postgres.New(postgres.Config{
