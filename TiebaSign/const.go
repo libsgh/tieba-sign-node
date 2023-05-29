97f8df7bbb21749c7a38a1e79aed49a443764dfd
libsgh <woiyyng@gmail.com>
2023-05-29 15:11:47 +0800

    优化

--- TiebaSign/const.go
+++ TiebaSign/const.go
@@ -6,7 +6,7 @@ const MainServer string = "http://localhost:8081"
 
 var DB_TYPE string = getEnv("DB_TYPE", "sqlite")
 var PORT string = getEnv("PORT", "8088")
-var NODE_NAME string = getEnv("PORT", "8088")
+var NODE_NAME string = getEnv("NODE_NAME", "http://localhost:8088")
 
 func getEnv(key, defaultValue string) string {
 	value := os.Getenv(key)
